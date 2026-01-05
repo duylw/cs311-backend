cde766ce0339019fc63b7db67cd4c1e4ad518de9
Duy <duylw25@gmail.com>
2026-01-05 22:31:39 +0700

    Redundant Remove

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -3,15 +3,13 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from app.core.config import settings
 from loguru import logger
 
-from dotenv import load_dotenv
-load_dotenv()
-
 class EmbeddingService:
     def __init__(self):
         self.embedding_models = {
             # settings.OLLAMA_EMBEDDING_MODEL_NAME: OllamaEmbeddings(model=settings.OLLAMA_EMBEDDING_MODEL_NAME),
             settings.GOOGLE_EMBEDDING_MODEL: GoogleGenerativeAIEmbeddings(model=settings.GOOGLE_EMBEDDING_MODEL,
-                                                                          api_key=settings.GOOGLE_AI_API_KEY.get_secret_value())
+                                                                          api_key=settings.GOOGLE_AI_API_KEY.get_secret_value(),
+                                                                          output_dimensionality=768)
         }
     def get_embedding(self, model_name: str):
         return self.embedding_models.get(model_name)
